264d718d28c48272c008744220efcd796a478c1d
JUAN SEBASTIAN SANCHEZ ROJAS <sanchez.iag2@gmail.com>
2020-08-03 18:38:39 -0600

    Add project files.

---
+++ MineSweeper/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace MineSweeper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Tablero = new System.Windows.Forms.DataGridView();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Tablero)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(650, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 76);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Tablero
+            // 
+            this.Tablero.AllowUserToAddRows = false;
+            this.Tablero.AllowUserToDeleteRows = false;
+            this.Tablero.AllowUserToOrderColumns = true;
+            this.Tablero.AllowUserToResizeColumns = false;
+            this.Tablero.AllowUserToResizeRows = false;
+            this.Tablero.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Tablero.BackgroundColor = System.Drawing.Color.Gray;
+            this.Tablero.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Tablero.ColumnHeadersVisible = false;
+            this.Tablero.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.Tablero.GridColor = System.Drawing.Color.Gray;
+            this.Tablero.Location = new System.Drawing.Point(1, 3);
+            this.Tablero.Name = "Tablero";
+            this.Tablero.RowHeadersVisible = false;
+            this.Tablero.RowHeadersWidth = 51;
+            this.Tablero.RowTemplate.Height = 24;
+            this.Tablero.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.Tablero.Size = new System.Drawing.Size(640, 446);
+            this.Tablero.TabIndex = 1;
+            this.Tablero.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.Tablero_CellClick);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(671, 188);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(84, 28);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "FLAG";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Click += new System.EventHandler(this.radioButton1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(649, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 80);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "🏴 FLAGS: \r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(215, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 51);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "YOU WIN";
+            this.label2.Visible = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "EASY",
+            "MEDIUM",
+            "HARD"});
+            this.comboBox1.Location = new System.Drawing.Point(650, 10);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(660, 237);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 107);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "🕑 TIME:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 451);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Tablero);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "MineSweeper";
+            ((System.ComponentModel.ISupportInitialize)(this.Tablero)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView Tablero;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ MineSweeper/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MineSweeper
+{
+    public partial class Form1 : Form
+    {
+        static int n, Tminas;
+        Random rnd = new Random();
+        int[,] TableroVals;
+        bool usarBandera = false;
+        int Descubierto, segundos, Banderas, fontS;
+        bool endCondition;
+        DataGridViewCellStyle styleGrass = new DataGridViewCellStyle();
+        DataGridViewCellStyle styleDig = new DataGridViewCellStyle();
+        DataGridViewCellStyle styleFlag = new DataGridViewCellStyle();
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Text = "EASY";
+        }
+        private void Inicio(int fSize)
+        {
+            segundos = 0;
+            Tablero.Rows.Clear();
+            Tablero.RowCount = n;
+            Tablero.ColumnCount = n;
+            styleGrass.BackColor = Color.FromArgb(81, 219, 199);
+            styleDig.BackColor = Color.FromArgb(147, 186, 180);
+            styleFlag.BackColor = Color.Red;
+            styleDig.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            styleFlag.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            Tablero.Font = new Font("Arial", fSize, FontStyle.Bold);
+            Tablero.ClearSelection();
+            for (int i = 0; i < n; i++)
+            {
+                DataGridViewColumn columna = Tablero.Columns[i];
+                DataGridViewRow fila = Tablero.Rows[i];
+                columna.Width = (int)(Tablero.Width / n);
+                fila.Height = (int)(Tablero.Height / n);
+                for (int j = 0; j < n; j++)
+                {
+                    TableroVals[j, i] = 0;
+                    Tablero[j, i].Style = styleGrass;
+                }
+            }
+            Banderas = Tminas;
+            label1.Text = "🏴 FLAGS: " + Banderas;
+            Minas(Tminas);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            endCondition = false;
+            timer1.Enabled = !endCondition;
+            switch (comboBox1.Text)
+            {
+                case "EASY":
+                    n = 9;
+                    Tminas = 10; 
+                    fontS = 16;
+                    break;
+                case "MEDIUM":
+                    n = 16;
+                    Tminas = 40;
+                    fontS = 12;
+                    break;
+                case "HARD":
+                    n = 24;
+                    Tminas = 99;
+                    fontS = 7;
+                    break;
+            }
+            label2.Visible = false;
+            label2.Text = "YOU WIN";
+            Descubierto = 0;
+            TableroVals = new int[n, n];
+            usarBandera = false;
+            radioButton1.Checked = usarBandera;
+            Inicio(fontS);
+        }
+        private bool PosValida(int x, int y)
+        {
+            return (x >= 0) && (x < n) && (y >= 0) && (y < n);
+        }
+        private void Minas(int cantidad)
+        {
+            int x, y;
+            for (int i = 0; i < cantidad; i++)
+            {
+                x = rnd.Next(0, n);
+                y = rnd.Next(0, n);
+                if (TableroVals[x, y] != 9)
+                {
+                    TableroVals[x, y] = 9;
+                    AsignarVal(x, y);
+                }
+                else
+                    i--;
+            }
+        }
+        private void AsignarVal(int x, int y)
+        {
+            if (PosValida(x + 1, y))
+                if (TableroVals[x + 1, y] != 9)
+                    TableroVals[x + 1, y]++;
+            if (PosValida(x - 1, y))
+                if (TableroVals[x - 1, y] != 9)
+                    TableroVals[x - 1, y]++;
+            if (PosValida(x, y + 1))
+                if (TableroVals[x, y + 1] != 9)
+                    TableroVals[x, y + 1]++;
+            if (PosValida(x, y - 1))
+                if (TableroVals[x, y - 1] != 9)
+                    TableroVals[x, y - 1]++;
+            if (PosValida(x + 1, y + 1))
+                if (TableroVals[x + 1, y + 1] != 9)
+                    TableroVals[x + 1, y + 1]++;
+            if (PosValida(x - 1, y - 1))
+                if (TableroVals[x - 1, y - 1] != 9)
+                    TableroVals[x - 1, y - 1]++;
+            if (PosValida(x + 1, y - 1))
+                if (TableroVals[x + 1, y - 1] != 9)
+                    TableroVals[x + 1, y - 1]++;
+            if (PosValida(x - 1, y + 1))
+                if (TableroVals[x - 1, y + 1] != 9)
+                    TableroVals[x - 1, y + 1]++;
+        }
+        private void Revelar(int x, int y)
+        {
+            if (TableroVals[x, y] == 9)
+            {
+                Tablero[x, y].Value = "💣";
+                Tablero[x, y].Style = styleFlag;
+                label2.Text = "YOU LOSE";
+                label2.Visible = true;
+                endCondition = true;
+                timer1.Enabled = !endCondition;
+            }
+            else
+            {
+                if (TableroVals[x, y] > 0)
+                {
+                    Tablero[x, y].Value = TableroVals[x, y];
+                    Tablero[x, y].Style = Colores(TableroVals[x, y]);
+                    TableroVals[x, y] = -1;
+                    Descubierto++;
+                }
+                else if (TableroVals[x, y] == 0)
+                    RevelarRec(x, y);
+            }
+        }
+        private void RevelarRec(int x, int y)
+        {
+            if (TableroVals[x, y] != -1)
+            {
+                if (TableroVals[x, y] > 0)
+                {
+                    Tablero[x, y].Value = TableroVals[x, y];
+                    Tablero[x, y].Style = Colores(TableroVals[x, y]);
+                    TableroVals[x, y] = -1;
+                }
+                else
+                {
+                    TableroVals[x, y] = -1;
+                    Tablero[x, y].Value = " ";
+                    Tablero[x, y].Style = styleDig;
+                    if (PosValida(x + 1, y))
+                        RevelarRec(x + 1, y);
+                    if (PosValida(x - 1, y))
+                        RevelarRec(x - 1, y);
+                    if (PosValida(x, y + 1))
+                        RevelarRec(x, y + 1);
+                    if (PosValida(x, y - 1))
+                        RevelarRec(x, y - 1);
+                    if (PosValida(x + 1, y + 1))
+                        RevelarRec(x + 1, y + 1);
+                    if (PosValida(x - 1, y - 1))
+                        RevelarRec(x - 1, y - 1);
+                    if (PosValida(x + 1, y - 1))
+                        RevelarRec(x + 1, y - 1);
+                    if (PosValida(x - 1, y + 1))
+                        RevelarRec(x - 1, y + 1);
+                }
+                Descubierto++;
+            }
+        }
+        private void Marcar(int x, int y)
+        {
+            if (Tablero[x, y].Style == styleFlag)
+            {
+                Tablero[x, y].Style = styleGrass;
+                Tablero[x, y].Value = " ";
+                Banderas++;
+            }
+            else if (Tablero[x, y].Style != styleDig && Banderas > 0)
+            {
+                Tablero[x, y].Style = styleFlag;
+                Tablero[x, y].Value = "🏴";
+                Banderas--;
+            }
+            label1.Text = "🏴 FLAGS: " + Banderas;
+        }
+        private DataGridViewCellStyle Colores(int val)
+        {
+            if (val == 1)
+            {
+                DataGridViewCellStyle style1 = new DataGridViewCellStyle();
+                style1.BackColor = styleDig.BackColor;
+                style1.ForeColor = Color.FromArgb(0, 60, 255);
+                style1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                return style1;
+            }
+            if (val == 2)
+            {
+                DataGridViewCellStyle style2 = new DataGridViewCellStyle();
+                style2.BackColor = styleDig.BackColor;
+                style2.ForeColor = Color.FromArgb(0, 200, 30);
+                style2.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                return style2;
+            }
+            if (val == 3)
+            {
+                DataGridViewCellStyle style3 = new DataGridViewCellStyle();
+                style3.BackColor = styleDig.BackColor;
+                style3.ForeColor = Color.FromArgb(232, 14, 14);
+                style3.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                return style3;
+            }
+            if (val == 4)
+            {
+                DataGridViewCellStyle style4 = new DataGridViewCellStyle();
+                style4.BackColor = styleDig.BackColor;
+                style4.ForeColor = Color.FromArgb(31, 42, 145);
+                style4.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                return style4;
+            }
+            if (val == 5)
+            {
+                DataGridViewCellStyle style5 = new DataGridViewCellStyle();
+                style5.BackColor = styleDig.BackColor;
+                style5.ForeColor = Color.FromArgb(145, 31, 31);
+                style5.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                return style5;
+            }
+            if (val == 6)
+            {
+                DataGridViewCellStyle style6 = new DataGridViewCellStyle();
+                style6.BackColor = styleDig.BackColor;
+                style6.ForeColor = Color.FromArgb(33, 156, 143);
+                style6.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                return style6;
+            }
+            if (val == 7)
+            {
+                DataGridViewCellStyle style7 = new DataGridViewCellStyle();
+                style7.BackColor = styleDig.BackColor;
+                style7.ForeColor = Color.FromArgb(35, 37, 41);
+                style7.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                return style7;
+            }
+            if (val == 8)
+            {
+                DataGridViewCellStyle style8 = new DataGridViewCellStyle();
+                style8.BackColor = styleDig.BackColor;
+                style8.ForeColor = Color.FromArgb(108, 128, 125);
+                style8.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                return style8;
+            }
+            return styleDig;
+        }
+        private void CheckWin()
+        {
+            if (Descubierto == ((int)Math.Pow(n, 2) - Tminas) && Banderas == 0)
+            {
+                label2.Visible = true;
+                endCondition = true;
+                timer1.Enabled = !endCondition;
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label3.Text = "🕑 TIME: \n" + segundos;
+            segundos++;
+        }
+        private void Tablero_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (!endCondition)
+            {
+                if (!usarBandera)
+                    Revelar(e.ColumnIndex, e.RowIndex);
+                else
+                    Marcar(e.ColumnIndex, e.RowIndex);
+                CheckWin();
+            }
+            Tablero.ClearSelection();
+        }
+        private void radioButton1_Click(object sender, EventArgs e)
+        {
+            usarBandera = !usarBandera;
+            radioButton1.Checked = usarBandera;
+        }
+    }
+}
